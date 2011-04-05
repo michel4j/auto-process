c0819257d3ccd730ea227aff73ce0f408d99b8e8
Michel Fodje <michel.fodje@lightsource.ca>
2011-04-05 14:40:38 -0600

    - forgot peaks file

---
+++ dpm/utils/peaks.py
@@ -0,0 +1,31 @@
+'''
+Created on Apr 5, 2011
+
+@author: michel
+'''
+
+
+import numpy
+from scipy.ndimage import measurements
+from scipy.ndimage import filters
+from scipy.misc import imread
+
+def detect_beam_peak(filename):
+    img_array = imread(filename, flatten=True)
+      
+    # filter the array so that features less than 8 pixels wide are blurred out
+    # assumes that beam center is at least 8 pixels wide
+    arr = filters.gaussian_filter(img_array, 8)
+    beam_y, beam_x = measurements.maximum_position(arr)
+    
+    # valid beam centers must be within the center 1/5 region of the detector surface
+    shape = img_array.shape
+    cmin = [2 * v/5 for v in shape]
+    cmax = [3 * v/5 for v in shape]
+    good = False
+    if cmin[0] < beam_y < cmax[0] and cmin[1] < beam_x < cmax[1]:
+        good = True
+        
+    return beam_x, beam_y, good
+    
+

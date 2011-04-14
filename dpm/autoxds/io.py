c5f4938ab571b57427a5c5010b25247289aa86e9
Michel Fodje <michel.fodje@lightsource.ca>
2011-04-13 19:58:25 -0600

    - bug fix in calculation of number of jobs for XDS.INP

--- dpm/autoxds/io.py
+++ dpm/autoxds/io.py
@@ -74,7 +74,7 @@ def write_xds_input(jobs, params):
         detector = 'CCDCHESS'
     
     #determine number of CPUS based on default DELPHI of 5 degrees and cpu_count
-    _num_cpus = int(round(DEFAULT_DELPHI / params['delta_angle']))
+    _num_cpus = int(round(DEFAULT_DELPHI / params['delta_angle'])) + 1
     _num_cpus = min(_num_cpus, params['cpu_count'])
     
     #determine max number of jobs cores available and _num_cpus

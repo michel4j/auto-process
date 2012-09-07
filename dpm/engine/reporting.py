0e18e4d3d60bd7342d09fce2dc0769171658120f
Michel Fodje <michel.fodje@lightsource.ca>
2012-09-07 12:52:38 -0600

    - bug fix

--- dpm/engine/reporting.py
+++ dpm/engine/reporting.py
@@ -577,7 +577,8 @@ def get_reports(datasets, options={}):
                 
             if dres['correction'].get('frame_statistics'):                     
                 _t = Table(dres['correction']['frame_statistics'])
-                for k in ['frame_no', 'i_obs', 'n_misfit', 'r_meas', 'i_sigma', 'unique', 'corr']:
+                _section['frame_no'] = _t['frame']
+                for k in ['i_obs', 'n_misfit', 'r_meas', 'i_sigma', 'unique', 'corr']:
                     _section[k] = _t[k]
                     
             if dres['correction'].get('diff_statistics'):

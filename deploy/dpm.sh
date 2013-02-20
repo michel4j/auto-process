760ae5e345f2ef0563a34cdb59d7ad5fbfb8c6f8
Michel Fodje <michel.fodje@lightsource.ca>
2013-02-20 16:32:24 -0600

    - added INSTALL file and deploy help

--- deploy/dpm.sh
+++ deploy/dpm.sh
@@ -3,8 +3,13 @@
 # Set DPM_PATH to the top-level directory containing the DPM modules
 
 export DPM_PATH=/cmcf_apps/dpm-3
+
+# Configure a list nodes in data processing cluster <hostname-or-ip>:<number of cores>
+# Each machine in this list must support password-less private key ssh authentication for the user"
+
 export DPM_HOSTS="srv-cmcf-dp1:32 srv-cmcf-dp2:32 srv-cmcf-dp3:32"
 
+# Do not change below here
 export PATH=${PATH}:$DPM_PATH/bin
 if [ $PYTHONPATH ]; then
 	export PYTHONPATH=${PYTHONPATH}:${DPM_PATH}

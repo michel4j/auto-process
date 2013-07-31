cc6c94d11553726b50cb7032bd1a515c9b64c3b8
Michel Fodje <michel.fodje@lightsource.ca>
2013-07-31 13:22:29 -0600

    - bug fixes

--- dpm/engine/solver.py
+++ dpm/engine/solver.py
@@ -6,7 +6,7 @@ _logger = log.get_module_logger(__name__)
 
 def solve_small_molecule(info, options={}):
     os.chdir(options.get('directory', '.'))
-    _logger.info("Attempting to solve small-molecule structure ...")
+    _logger.info("Solving small-molecule structure ...")
     if not misc.file_requirements('%s-shelx.hkl' % info['name']):
         print "File not found %s-shelx.hkl" % info['name']
         return {'step': 'symmetry', 'success': False, 'reason': 'Required reflection files missing'}
@@ -16,11 +16,10 @@ def solve_small_molecule(info, options={}):
     except (dpm.errors.ProcessError, dpm.errors.ParserError, IOError), e:
         return {'step': 'symmetry', 'success': False, 'reason': str(e)}
     
-    
-    _logger.info('Succeeded! Coordinates: %s.res, Phases: %s.fcf' % (
-                                        os.path.join('shelx-sm', info['name']),
-                                        os.path.join('shelx-sm', info['name']),
-                                        ))
+    _smx_dir = misc.relpath(
+                    os.path.join(options.get('directory', '.'), 'shelx-sm', info['name']), 
+                    options.get('command_dir'))
+    _logger.info('Coordinates: %s.res, Phases: %s.fcf' % (_smx_dir, _smx_dir))
 
 
     return {'step':'smx_structure', 'success':True, 'data': None}   

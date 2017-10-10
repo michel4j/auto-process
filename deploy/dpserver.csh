dabb27eeb66ec352edaebf223f299dc0cc69653d
Michel Fodje <michel.fodje@lightsource.ca>
2017-10-10 12:05:48 -0600

    fixed environment setup

--- deploy/dpserver.csh
+++ deploy/dpserver.csh
@@ -1,5 +1,6 @@
 #!/bin/csh
-
-set path=($path /cmcf_apps/autoprocess-3.3.1/bin)
 setenv DPS_HOSTS "HPC1608-001:112 HPC1608-002:112 HPC1608-002:112"
+setenv DPS_PATH /cmcf_apps/AutoProcess-4
+
+set path=($path $DPS_PATH/bin)
 setenv LC_ALL en_US.UTF8
--- deploy/dpserver.sh
+++ deploy/dpserver.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-export PATH=${PATH}:/cmcf_apps/autoprocess-3.3.1/bin
 export DPS_NODES="HPC1608-001:112 HPC1608-002:112 HPC1608-002:112"
+export DPS_PATH=/cmcf_apps/AutoProcess-4
+
+export PATH=${PATH}:$DPS_PATH/bin
 export LC_ALL=en_US.UTF8

3228416de17cc0da55844f1419e3677310fe9552
Michel Fodje <michel.fodje@lightsource.ca>
2018-03-17 17:24:29 -0600

    Bug fix

--- autoprocess/engine/powder.py
+++ autoprocess/engine/powder.py
@@ -414,7 +414,7 @@ class FrameAnalyser(object):
                 logger.info('Running calibration through fit2d ...')
                 if os.path.exists(self.db_file):
                     logger.warning('Calibration file will be overwritten')
-                output = subprocess.check_output(args, timeout=120)
+                output = subprocess.check_output(args, timeout=120, stderr=subprocess.STDOUT)
                 os.remove('calib.mac')
             data_file = '{}.chi'.format(params['data_name'])
             if os.path.exists(data_file):
@@ -449,7 +449,7 @@ class FrameAnalyser(object):
                 fitio.write_integrate_macro(params, macro_file='integrate.mac')
                 with Xvfb() as xvfb:
                     args = ['fit2d', '-dim{0:0.0f}x{0:0.0f}'.format(dim), '-macintegrate.mac']
-                    output = subprocess.check_output(args, timeout=120)
+                    output = subprocess.check_output(args, timeout=120, stderr=subprocess.STDOUT)
                     os.remove('integrate.mac')
             data_file = '{}.chi'.format(params['data_name'])
             if os.path.exists(data_file):

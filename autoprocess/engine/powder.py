4ddd2019fda1d1b0986433055bb957673a6a8589
Michel Fodje <michel.fodje@lightsource.ca>
2018-03-22 09:13:05 -0600

    Bug fix

--- autoprocess/engine/powder.py
+++ autoprocess/engine/powder.py
@@ -411,12 +411,12 @@ class FrameAnalyser(object):
             fitio.write_calib_macro(params, macro_file='calib.mac')
 
             with Xvfb() as xvfb:
-                args = ['fit2d', '-dim{0:0.0f}x{0:0.0f}'.format(dim), '-maccalib.mac']
+                args = ['/cmcf_apps/bin/fit2d', '-dim{0:0.0f}x{0:0.0f}'.format(dim), '-maccalib.mac']
                 logger.info('Running calibration through fit2d ...')
                 if os.path.exists(self.db_file):
                     logger.warning('Calibration file will be overwritten')
                 output = subprocess.check_output(args, timeout=120, stderr=subprocess.STDOUT)
-                os.remove('calib.mac')
+            os.remove('calib.mac')
             data_file = '{}.chi'.format(params['data_name'])
             if os.path.exists(data_file):
                 data = numpy.loadtxt(data_file, skiprows=4)
@@ -449,9 +449,8 @@ class FrameAnalyser(object):
                 fitio.write_integrate_macro(params, macro_file='integrate.mac')
                 with Xvfb() as xvfb:
                     logger.info('Integrating frame {}: {} ...'.format(self.group_name, self.frame_name))
-                    args = ['fit2d', '-dim{0:0.0f}x{0:0.0f}'.format(dim), '-macintegrate.mac']
+                    args = ['/cmcf_apps/bin/fit2d', '-dim{0:0.0f}x{0:0.0f}'.format(dim), '-macintegrate.mac']
                     output = subprocess.check_output(args, timeout=120, stderr=subprocess.STDOUT)
-                    logger.info(output)
                 os.remove('integrate.mac')
             data_file = '{}.chi'.format(params['data_name'])
             if os.path.exists(data_file):

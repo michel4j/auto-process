69818e950790cecaeb02cfe70b25d00990c3309e
Michel Fodje <michel.fodje@lightsource.ca>
2019-02-07 13:42:47 -0600

    Set logging to INFO level

--- autoprocess/utils/log.py
+++ autoprocess/utils/log.py
@@ -4,7 +4,7 @@ import logging
 import repr as reprlib
 from logging.handlers import RotatingFileHandler
 
-LOG_LEVEL = logging.DEBUG
+LOG_LEVEL = logging.INFO
 IMPORTANT = 25
 logging.addLevelName(IMPORTANT, 'IMPORTANT')
 DEBUGGING = False

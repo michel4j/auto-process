8cf862ff2ce49e8e8c7392442b9dd31beca6f8e7
Michel Fodje <michel.fodje@lightsource.ca>
2019-03-07 16:00:19 -0600

    Import latest imageio

--- autoprocess/utils/dataset.py
+++ autoprocess/utils/dataset.py
@@ -13,7 +13,7 @@ from autoprocess.libs.imageio import read_header, read_image
 from autoprocess.utils import misc
 from autoprocess.utils.log import get_module_logger
 
-_logger = get_module_logger(__name__)
+logger = get_module_logger(__name__)
 
 
 def _all_files(root, patterns='*'):
@@ -61,113 +61,90 @@ def import_parameters(inp_file):
     return get_parameters(img_file)
 
 
+def summarize_list(full_frame_set):
+    # takes a list of integers such as [1,2,3,4,6,7,8]
+    # and reduces it to the string [(1,4),(6,8)]
+    sum_list = []
+    tmp_pairs = []
+    full_set = list(set(full_frame_set))
+
+    if len(full_set) == 0:
+        return ""
+    full_set.sort()
+    cur = full_set.pop(0)
+    tmp_pairs.append([cur, cur])
+    while len(full_set) > 0:
+        cur = full_set.pop(0)
+        last_pair = tmp_pairs[-1]
+        if (cur - last_pair[-1]) == 1:
+            last_pair[-1] = cur
+        else:
+            tmp_pairs.append([cur, cur])
+
+    return tmp_pairs
+
+
+def summarize_gaps(frame_list):
+    # takes a list of integers such as [1,2,3,4,7,8]
+    # and reduces it to the string of skipped regions such as "[(5,6)]"
+
+    complete_set = set(range(1, max(frame_list) + 1))
+    frame_set = set(frame_list)
+    full_set = list(complete_set.difference(frame_set))
+    return summarize_list(full_set)
+
+
 def get_parameters(img_file):
     """ 
     Determine parameters for the data set represented by img_fiile
     returns a dictionary of results
     
     """
-    directory, filename = os.path.split(os.path.abspath(img_file))
+    data = read_image(os.path.abspath(img_file))
 
-    file_pattern = re.compile('^(?P<base>.+_)(?P<num>\d{3,6})(?P<ext>\.?[\w.]+)?$')
-    fm = file_pattern.match(filename)
-    if fm:
-        if fm.group('ext') is not None:
-            extension = fm.group('ext')
-        else:
-            extension = ''
-        filler = '?' * len(fm.group('num'))
-        xds_template = "%s%s%s" % (fm.group('base'), filler, extension)
-    else:
-        _logger.error("Filename `%s` not recognized as dataset." % filename)
-        raise autoprocess.errors.DatasetError('Filename not recognized')
-
-    file_list = list(_all_files(directory, xds_template))
-    frame_count = len(file_list)
-    if frame_count == 0:
-        _logger.error("Dataset not found")
+    if not data.header['dataset'] or len(data.header['dataset']['sequence']) == 0:
+        logger.error("Dataset not found")
         raise autoprocess.errors.DatasetError('Dataset not found')
 
-    fm = file_pattern.search(file_list[0])
-    first_frame = int(fm.group('num'))
-    _dataset_name = fm.group('base')
-    if _dataset_name[-1] in ['_', '.', '-']:
-        _dataset_name = _dataset_name[:-1]
-
-    reference_image = os.path.join(directory, file_list[0])
+    reference_image = data.header['dataset']['name'].format(data.header['dataset']['sequence'][0])
     if not (os.path.isfile(reference_image) and os.access(reference_image, os.R_OK)):
-        _logger.info("File '%s' not found, or unreadable." % reference_image)
+        logger.info("File '%s' not found, or unreadable." % reference_image)
         sys.exit(1)
 
-    info = read_header(reference_image)
-    _overwrite_beam = False
-    if first_frame == 0:
-        first_frame = 1
-        _ow_beam_x, _ow_beam_y, _overwrite_beam = detect_beam_peak(os.path.join(directory, file_list[0]))
-        if _overwrite_beam:
-            _logger.info('%s: New beam origin from frame 000 [%d, %d].' % (_dataset_name, _ow_beam_x, _ow_beam_y))
-            info['beam_center'] = (_ow_beam_x, _ow_beam_y)
-        file_list = file_list[1:]
-        frame_count = len(file_list)
-
+    data = read_image(reference_image)
+    info = data.header
     info['energy'] = misc.wavelength_to_energy(info['wavelength'])
-    info['first_frame'] = first_frame
-    info['frame_count'] = frame_count
-    info['name'] = _dataset_name
-    info['file_template'] = os.path.join(directory, xds_template)
-
+    info['first_frame'] = info['dataset']['sequence'][0]
+    info['frame_count'] = info['dataset']['sequence'][-1]
+    info['name'] = info['dataset']['label']
+    info['file_template'] = os.path.join(info['dataset']['directory'], info['dataset']['template'])
 
     # Generate a list of wedges. each wedge is a tuple. The first value is the
     # first frame number and the second is the number of frames in the wedge
-    wedges = []
-    _wedge = [0, 0]
-    for i, f in enumerate(file_list):
-        _fm = file_pattern.match(f)
-        _fn = int(_fm.group('num'))
-        _frame = (f, _fn)
-        if i == 0:
-            _wedge = [_fn, 1]
-        else:
-            if (_wedge[0] + _wedge[1]) == _fn:
-                _wedge[1] += 1
-            else:
-                if (_wedge[1]) > 0:
-                    wedges.append(_wedge)
-                    _wedge = [_fn, 1]
-    if (_wedge[0]) > 0:
-        wedges.append(_wedge)
+    wedges = summarize_list(info['dataset']['sequence'])
 
     # determine spot ranges from wedges
     # up to 4 degrees per wedge starting at 0 and 45 and 90
-
     spot_range = []
-    _spot_span = int(4.0 // info['delta_angle'])  # frames in 4 deg
-    _first_wedge = wedges[0]
+    spot_span = int(4.0 // info['delta_angle'])  # frames in 4 deg
+    first_wedge = wedges[0]
 
     for _ang in [0.0, 45.0, 90.0]:
-        _rs = _first_wedge[0] + int(_ang // info['delta_angle'])
-        _re = _rs + _spot_span
+        _rs = first_wedge[0] + int(_ang // info['delta_angle'])
+        _re = _rs + spot_span
         _exp_set = set(range(_rs, _re))
         for wedge in wedges:
             _obs_set = set(range(wedge[0], wedge[0] + wedge[1]))
             _range = (_exp_set & _obs_set)
             if len(_range) > 0:
                 spot_range.append((min(_range), max(_range)))
-    last_frame = wedges[-1][0] + wedges[-1][1] - 1
-
-    missing = []
-    for i, wedge in enumerate(wedges):
-        if i > 0:
-            _re = wedge[0] - 1
-            missing.append([_rs, _re])
-        _rs = wedge[0] + wedge[1]
 
     biggest_wedge = sorted(wedges, key=lambda x: x[1], reverse=True)[0]
 
     info['spot_range'] = spot_range
-    info['data_range'] = (first_frame, last_frame)
+    info['data_range'] = info['dataset']['sequence'][0], info['dataset']['sequence'][-1]
     info['reference_image'] = reference_image
     info['background_range'] = (biggest_wedge[0], biggest_wedge[0] + min(10, biggest_wedge[1]) - 1)
-    info['skip_range'] = missing
+    info['skip_range'] = summarize_gaps(info['dataset']['sequence'])
     info['max_delphi'] = info['delta_angle'] * biggest_wedge[1]
     return info
